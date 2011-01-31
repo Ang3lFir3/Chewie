a432a318be7d35311ef200423f8b4f1889648a6b
ang3lfir3 <ang3lfir3@gmail.com>
2011-01-31 08:40:16 -0800

    Getting name and version specfications working

--- chewie.ps1
+++ chewie.ps1
@@ -1,1 +1,19 @@
-$packages = gc .\.NugetFile | Foreach-Object {nuget install $_}
+function nuget 
+{
+	[CmdletBinding()]
+	param(
+		[Parameter(Position=0,Mandatory=1)]
+		[string] $name = $null,
+		
+		[Parameter(Position=1,Mandatory=0)]
+		[string] $version = ""
+		)
+		
+		$command = "nuget.exe install $name"
+		if($version -ne ""){$command += " -v $version"}
+		
+	invoke-expression $command
+
+}
+
+gc .\.NugetFile | Foreach-Object { $block = [scriptblock]::Create($_.ToString()); % $block;}
