d13d55506714967b1b999a529103eab5ae2bcc90
ang3lfir3 <ang3lfir3@gmail.com>
2011-02-03 17:17:57 -0800

    Added support for setting the install location.... the latest version of nuget no longer puts the packages in a packages dir.... make sure you $> nuget update .... before using

--- chewie.ps1
+++ chewie.ps1
@@ -1,3 +1,13 @@
+function install_to
+{
+	param(
+		[Parameter(Position=0,Mandatory=1)]
+		[string] $path = $null
+		)
+	if(!(test-path $path)){new-item -path . -name $path -itemtype directory}
+	push-location $path -stackname 'chewie_nuget'
+}
+
 function nuget 
 {
 	[CmdletBinding()]
@@ -17,3 +27,4 @@ function nuget
 }
 
 gc $pwd\.NugetFile | Foreach-Object { $block = [scriptblock]::Create($_.ToString()); % $block;}
+if((get-location -stackname 'chewie_nuget').count -gt 0) {pop-location -stackname 'chewie_nuget'}
