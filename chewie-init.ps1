251ff815b216e2fe4949c12ef276d9472ac5f4d5
Ang3lFir3 <ang3lfir3@gmail.com>
2012-04-03 17:16:36 -0700

    update the init script to include a choclatey pakcage

--- chewie-init.ps1
+++ chewie-init.ps1
@@ -3,6 +3,7 @@ function make_a_new_nugetFile
 	new-item -path $pwd -name .NugetFile -itemtype file
 	add-content $pwd\.NugetFile "install_to 'lib'"
 	add-content $pwd\.NugetFile "chew 'machine.specifications'"
+	add-content $pwd\.NugetFile "chocolate 'launchy'"
  }
  
 if(!(test-path $pwd\.NugetFile)){make_a_new_nugetFile}
