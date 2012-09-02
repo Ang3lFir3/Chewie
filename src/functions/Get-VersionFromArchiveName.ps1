9de83b74f95aca08678db8013e0256814084958a
Ian Davis <ian.f.davis@gmail.com>
2012-09-02 07:07:43 -0700

    Adding additional digit to version and making patch optional to support most nuget packages since they don't support semver

--- src/functions/Get-VersionFromArchiveName.ps1
+++ src/functions/Get-VersionFromArchiveName.ps1
@@ -6,10 +6,13 @@ function Get-VersionFromArchiveName {
     [Parameter(Position=1,Mandatory=$true)] [string]$archiveFile
   )
   $file = Split-Path -Leaf $archiveFile
-  $semver = '(\d+\.\d+\.\d+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?'
-  if($file -imatch "^$dependencyName\.$semver\.nupkg$" -and $matches.Keys.Count -gt 1) {
+  # Many packages don't support semver
+  #$semver = '(\d+\.\d+\.\d+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?'
+  # So the patch and fourth are optional :/
+  $version = '(\d+\.\d+(?:\.\d+)?(?:\.\d+)?)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?'
+  if($file -imatch "$version\.nupkg$" -and $matches.Keys.Count -gt 1) {
     # $matches[0] is the full file name
-    # $matches[1] has the major, minor, and patch version 1.2.3
+    # $matches[1] has the major, minor, and patch version 1.2[.3][.4]
     # $matches[2] is the pre-release version 
     # $matches[3] is the build version
     $versionString = $matches[1]
