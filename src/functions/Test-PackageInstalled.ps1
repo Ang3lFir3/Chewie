91d10ede4288ece59b08f32f881843b80f8e7df5
Ian Davis <ian@innovatian.com>
2012-09-09 08:43:55 -0700

    Adding code and tests for Test-PackageInstalled

--- src/functions/Test-PackageInstalled.ps1
+++ src/functions/Test-PackageInstalled.ps1
@@ -1,4 +1,25 @@
 
 function Test-PackageInstalled {
-
+  [CmdletBinding()]
+  param(
+    [Parameter(Position=0,Mandatory=$true)] [string]$packageName
+  )
+  $directoryNames = gci $chewie.default_source | ? { $_.PSIsContainer } | %{ $_.Name }
+  if($chewie.version_packages) {
+    $directoryNames = $directoryNames | % {
+      $version = Get-VersionFromString $_
+      if($version -ne $null) {
+        $_ = $_.Replace($version.Version.ToString(),"")
+        if(-not [string]::IsNullOrEmpty($version.Pre)) {
+          $_ = $_.Replace($version.Pre,"")
+        }
+        if(-not [string]::IsNullOrEmpty($version.Build)) {
+          $_ = $_.Replace($version.Build,"")
+        }
+        $_ = $_.Trim('.', '-', '+')
+      }
+      $_
+    }
+  }
+  return (@($directoryNames | ? {$_ -eq $packageName}).Length -gt 0)
 }
--- src/tests/Test-PackageInstalled.Tests.ps1
+++ src/tests/Test-PackageInstalled.Tests.ps1
@@ -3,11 +3,59 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -ireplace ".tests.", "."
 #. "$here\_Common.ps1"
 . "$here\..\functions\$sut"
 . "$here\..\functions\Assert-Condition.ps1"
+. "$here\..\functions\Get-VersionFromString.ps1"
+. "$here\..\functions\New-NuGetVersion.ps1"
 
+Describe "Ensure-InstalledPackagesCanBeFoundWhenNoVersionInformationIsGiven" {
+  . "$here\_Common.ps1"
+  $chewie.default_source = "TestDrive:"
+  Setup -Dir "Ninject"
+  It "should find the package" {
+    (Test-PackageInstalled "Ninject").should.be("true")
+  }
+  Cleanup
+}
 
-#Describe "Ensure-" {
-#  . "$here\_Common.ps1"
-#  It "should " {
-#    $pwd.should.be("default")
-#  }
-#}
+Describe "Ensure-InstalledPackagesCanBeFoundWhenASimpleVersionIsGiven" {
+  . "$here\_Common.ps1"
+  $chewie.default_source = "TestDrive:\"
+  $chewie.version_packages = $true
+  Setup -Dir "Ninject.2.3"
+  It "should find the package" {
+    (Test-PackageInstalled "Ninject").should.be("true")
+  }
+  Cleanup
+}
+
+Describe "Ensure-InstalledPackagesCanBeFoundWhenVersionWithDashedPrereleaseIsGiven" {
+  . "$here\_Common.ps1"
+  $chewie.default_source = "TestDrive:\"
+  $chewie.version_packages = $true
+  Setup -Dir "Ninject.2.3-alpha"
+  It "should find the package" {
+    (Test-PackageInstalled "Ninject").should.be("true")
+  }
+  Cleanup
+}
+
+Describe "Ensure-InstalledPackagesCanBeFoundWhenVersionWithPrereleaseIsGiven" {
+  . "$here\_Common.ps1"
+  $chewie.default_source = "TestDrive:\"
+  $chewie.version_packages = $true
+  Setup -Dir "Ninject.2.3alpha"
+  It "should find the package" {
+    (Test-PackageInstalled "Ninject").should.be("true")
+  }
+  Cleanup
+}
+
+Describe "Ensure-InstalledPackagesCanBeFoundWhenVersionWithBuildIsGiven" {
+  . "$here\_Common.ps1"
+  $chewie.default_source = "TestDrive:\"
+  $chewie.version_packages = $true
+  Setup -Dir "Ninject.2.3+1.0"
+  It "should find the package" {
+    (Test-PackageInstalled "Ninject").should.be("true")
+  }
+  Cleanup
+}
