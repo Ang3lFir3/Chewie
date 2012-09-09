eacfe87bb510b166bfe9e8706d82e472b5978776
Ian Davis <ian@innovatian.com>
2012-09-09 07:42:59 -0700

    Adding tests for Test-VersionCompatibility to figure out if we are outdated and the available version is allowed

---
+++ src/functions/Test-VersionCompatibility.ps1
@@ -0,0 +1,42 @@
+﻿function Test-VersionCompatibility {
+  [CmdletBinding()]
+  param(
+    [Parameter(Position=0,Mandatory=$true)] [string]$versionSpec,
+    [Parameter(Position=1,Mandatory=$true)] [string]$versionString
+  )
+  if([string]::IsNullOrEmpty($versionSpec)) {return true}
+  if($versionSpec.StartsWith('(')) {
+   $exlusiveLowerBound = $true
+  }
+  if($versionSpec.EndsWith(')')) {
+   $exlusiveUpperBound = $true
+  }
+  $exactMatch = $versionSpec.StartsWith('[') -and $versionSpec.EndsWith(']') -and ($versionSpec.Contains(',') -eq $false)
+  $versionSpec = $versionSpec.Trim('[',']','(',')')
+  $lower,$upper = $versionSpec.Split(',')
+  
+  Assert (!($exlusiveLowerBound -and $exlusiveUpperBound -and [string]::IsNullOrEmpty($upper) -and ($versionSpec.Contains(',') -eq $false))) ($messages.error_invalid_version_spec -f $versionSpec)
+  $current = (Get-VersionFromString $versionString).Version
+  if([string]::IsNullOrEmpty($lower)) {$lower = (New-NuGetVersion "0.0").Version}
+  if([string]::IsNullOrEmpty($upper)) {
+    if($exactMatch) {
+      $upper = $lower
+    } else {
+      $upper = (New-NuGetVersion "$([int]::MaxValue).$([int]::MaxValue).$([int]::MaxValue).$([int]::MaxValue)").Version
+    }
+  }
+  
+  if($exlusiveLowerBound) {
+    if($exlusiveUpperBound) {
+      return $lower -lt $current -and $current -lt $upper
+    } else {
+      return $lower -lt $current -and $current -le $upper
+    }
+  } else {
+    if($exlusiveUpperBound) {
+      return $lower -le $current -and $current -lt $upper
+    } else {
+      return $lower -le $current -and $current -le $upper
+    }
+  }
+}
---
+++ src/tests/Test-VersionCompatibility.Tests.ps1
@@ -0,0 +1,167 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -ireplace ".tests.", "."
+. "$here\_Common.ps1"
+. "$here\..\functions\$sut"
+. "$here\..\functions\Assert-Condition.ps1"
+. "$here\..\functions\New-NuGetVersion.ps1"
+. "$here\..\functions\Get-VersionFromString.ps1"
+
+Describe "Ensure-1.0 matches appropriately" {
+  . "$here\_Common.ps1"
+  $versionSpec = "1.0"
+  It "should exclude versions less $versionSpec" {
+    (Test-VersionCompatibility $versionSpec "0.9").should.be($false)
+  }
+  It "should include versions equal to $versionSpec" {
+    (Test-VersionCompatibility $versionSpec "1.0").should.be($true)
+  }
+  It "should include versions greater than $versionSpec" {
+    (Test-VersionCompatibility $versionSpec "1.1").should.be($true)
+  }
+}
+
+Describe "Ensure-(,1.0] matches appropriately" {
+  . "$here\_Common.ps1"
+  $versionSpec = "(,1.0]"
+  It "should include versions less than $versionSpec" {
+    (Test-VersionCompatibility $versionSpec "0.9").should.be($true)
+  }
+  It "should include versions equal to $versionSpec" {
+    (Test-VersionCompatibility $versionSpec "1.0").should.be($true)
+  }
+  It "should exclude versions greater than $versionSpec" {
+    (Test-VersionCompatibility $versionSpec "1.1").should.be($false)
+  }
+}
+
+Describe "Ensure-(,1.0) matches appropriately" {
+  . "$here\_Common.ps1"
+  $versionSpec = "(,1.0)"
+  It "should include versions less than $versionSpec" {
+    (Test-VersionCompatibility $versionSpec "0.9").should.be($true)
+  }
+  It "should exclude versions equal to $versionSpec" {
+    (Test-VersionCompatibility $versionSpec "1.0").should.be($false)
+  }
+  It "should exclude versions greater than $versionSpec" {
+    (Test-VersionCompatibility $versionSpec "1.1").should.be($false)
+  }
+}
+
+Describe "Ensure-[1.0] matches appropriately" {
+  . "$here\_Common.ps1"
+  $versionSpec = "[1.0]"
+  It "should exclude versions less than $versionSpec" {
+    (Test-VersionCompatibility $versionSpec "0.9").should.be($false)
+  }
+  It "should include versions equal to $versionSpec" {
+    (Test-VersionCompatibility $versionSpec "1.0").should.be($true)
+  }
+  It "should exclude versions greater than $versionSpec" {
+    (Test-VersionCompatibility $versionSpec "1.1").should.be($false)
+  }
+}
+
+Describe "Ensure-(1.0) matches appropriately" {
+  . "$here\_Common.ps1"
+  $versionSpec = "(1.0)"
+  It "should include versions less than $versionSpec" {
+    $error = $null
+    try{(Test-VersionCompatibility $versionSpec "0.9")}catch{$error = $_}
+    ($error -eq $null).should.be($false)
+  }
+}
+
+Describe "Ensure-(1.0,) matches appropriately" {
+  . "$here\_Common.ps1"
+  $versionSpec = "(1.0,)"
+  It "should exclude versions less than $versionSpec" {
+    (Test-VersionCompatibility $versionSpec "0.9").should.be($false)
+  }
+  It "should exclude versions equal to $versionSpec" {
+    (Test-VersionCompatibility $versionSpec "1.0").should.be($false)
+  }
+  It "should include versions greater than $versionSpec" {
+    (Test-VersionCompatibility $versionSpec "1.1").should.be($true)
+  }
+}
+
+Describe "Ensure-(1.0,2.0) matches appropriately" {
+  . "$here\_Common.ps1"
+  $versionSpec = "(1.0,2.0)"
+  It "should exclude versions less than $versionSpec lower bound" {
+    (Test-VersionCompatibility $versionSpec "0.9").should.be($false)
+  }
+  It "should exclude versions equal to $versionSpec lower bound" {
+    (Test-VersionCompatibility $versionSpec "1.0").should.be($false)
+  }
+  It "should include versions greater than $versionSpec lower bound and less than $versionSpec upper bound" {
+    (Test-VersionCompatibility $versionSpec "1.1").should.be($true)
+  }
+  It "should exclude versions equal to $versionSpec upper bound" {
+    (Test-VersionCompatibility $versionSpec "2.0").should.be($false)
+  }
+  It "should exclude versions greater than upper bound" {
+    (Test-VersionCompatibility $versionSpec "2.1").should.be($false)
+  }
+}
+
+Describe "Ensure-(1.0,2.0] matches appropriately" {
+  . "$here\_Common.ps1"
+  $versionSpec = "(1.0,2.0]"
+  It "should exclude versions less than $versionSpec lower bound" {
+    (Test-VersionCompatibility $versionSpec "0.9").should.be($false)
+  }
+  It "should exclude versions equal to $versionSpec lower bound" {
+    (Test-VersionCompatibility $versionSpec "1.0").should.be($false)
+  }
+  It "should include versions greater than $versionSpec lower bound and less than $versionSpec upper bound" {
+    (Test-VersionCompatibility $versionSpec "1.1").should.be($true)
+  }
+  It "should include versions equal to $versionSpec upper bound" {
+    (Test-VersionCompatibility $versionSpec "2.0").should.be($true)
+  }
+  It "should exclude versions greater than upper bound" {
+    (Test-VersionCompatibility $versionSpec "2.1").should.be($false)
+  }
+}
+
+Describe "Ensure-[1.0,2.0) matches appropriately" {
+  . "$here\_Common.ps1"
+  $versionSpec = "[1.0,2.0)"
+  It "should exclude versions less than $versionSpec lower bound" {
+    (Test-VersionCompatibility $versionSpec "0.9").should.be($false)
+  }
+  It "should include versions equal to $versionSpec lower bound" {
+    (Test-VersionCompatibility $versionSpec "1.0").should.be($true)
+  }
+  It "should include versions greater than $versionSpec lower bound and less than $versionSpec upper bound" {
+    (Test-VersionCompatibility $versionSpec "1.1").should.be($true)
+  }
+  It "should exclude versions equal to $versionSpec upper bound" {
+    (Test-VersionCompatibility $versionSpec "2.0").should.be($false)
+  }
+  It "should exclude versions greater than upper bound" {
+    (Test-VersionCompatibility $versionSpec "2.1").should.be($false)
+  }
+}
+
+Describe "Ensure-[1.0,2.0] matches appropriately" {
+  . "$here\_Common.ps1"
+  $versionSpec = "[1.0,2.0]"
+  It "should exclude versions less than $versionSpec lower bound" {
+    (Test-VersionCompatibility $versionSpec "0.9").should.be($false)
+  }
+  It "should include versions equal to $versionSpec lower bound" {
+    (Test-VersionCompatibility $versionSpec "1.0").should.be($true)
+  }
+  It "should include versions greater than $versionSpec lower bound and less than $versionSpec upper bound" {
+    (Test-VersionCompatibility $versionSpec "1.1").should.be($true)
+  }
+  It "should include versions equal to $versionSpec upper bound" {
+    (Test-VersionCompatibility $versionSpec "2.0").should.be($true)
+  }
+  It "should exclude versions greater than upper bound" {
+    (Test-VersionCompatibility $versionSpec "2.1").should.be($false)
+  }
+}
