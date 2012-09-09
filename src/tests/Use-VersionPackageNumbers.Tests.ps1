17eefd815e2946fcc68cff372cd31f1f651252c4
Ian Davis <ian@innovatian.com>
2012-09-09 04:08:47 -0700

    adding VersionPackages tests

--- src/tests/Use-VersionPackageNumbers.Tests.ps1
+++ src/tests/Use-VersionPackageNumbers.Tests.ps1
@@ -5,9 +5,24 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -ireplace ".tests.", "."
 . "$here\..\functions\Assert-Condition.ps1"
 
 
-#Describe "Ensure-" {
-#  . "$here\_Common.ps1"
-#  It "should " {
-#    $pwd.should.be("default")
-#  }
-#}
+Describe "Ensure-PackageVersionNumbersCanBeEnabled" {
+  . "$here\_Common.ps1"
+  It "should be off by default" {
+    $chewie.version_packages.should.be($false)
+  }
+  It "should be turned on with Use-VersionPackageNumbers" {
+    $chewie.version_packages = $false
+    Use-VersionPackageNumbers
+    $chewie.version_packages.should.be($true)
+  }
+  It "should be turned on with Version_Packages" {
+    $chewie.version_packages = $false
+    Version_Packages
+    $chewie.version_packages.should.be($true)
+  }
+  It "should be turned on with VersionPackages" {
+    $chewie.version_packages = $false
+    VersionPackages
+    $chewie.version_packages.should.be($true)
+  }
+}
