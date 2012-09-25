e772c01be70885a475766687a0ac47f89360c149
Ian Davis <ian.f.davis@gmail.com>
2012-09-25 13:23:34 -0700

    Adding new scipts for generating and updating nuspec files based on the .NuGetFile configuration. The implementation is just stubbed at this point while evaluating their usefulness.

---
+++ src/functions/New-NuSpecFile.ps1
@@ -0,0 +1,6 @@
+﻿
+function New-NuSpecFile {
+  param($fileName, $nugetFile = ".\.NuGetFile")
+  # parse the .NuGetFile to build the dependency tree,
+  # add a few other parameters for generating a template.
+}
---
+++ src/functions/Update-NuSpecDependencies.ps1
@@ -0,0 +1,6 @@
+﻿
+function Update-NuSpecDependencies {
+  param($fileName, $nugetFile = ".\.NuGetFile")
+  # parse the .NuGetFile to build the dependency tree
+  # get the current spec file as xml and replace the dependencies node.
+}
