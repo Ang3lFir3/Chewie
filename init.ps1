5751f8c2ee2f99334d3cdaedc48dcb575b2c3354
Ian Davis <ian.f.davis@gmail.com>
2012-09-25 15:54:56 -0700

    Adding script to install chewie into the user's module path. Locking dependency versions so that nothing breaks right now.

--- init.ps1
+++ init.ps1
@@ -1,9 +1,59 @@
 param($rootPath, $toolsPath, $package, $project)
 
-$chewieModule = "$rootPath\chewie.psm1"
-Write-Host "Importing $chewieModule"
-import-module -Force -Global $chewieModule
+function Install-Module {
+  param(
+    [Parameter(Position=0,Mandatory=$true)][string]$name,
+    [Parameter(Position=1,Mandatory=$true)][string]$directory
+  )
 
+  $path = New-Object System.IO.DirectoryInfo(Resolve-Path $directory)
+  Assert-PathExists $path
+  $modulesDirectory = Get-ModulesDirectoryForCurrentUser
+  Install-ModuleToPath $name $path $modulesDirectory
+}
+
+function Install-ModuleToPath {
+  param(
+    [Parameter(Position=0,Mandatory=$true)][string]$name,
+    [Parameter(Position=1,Mandatory=$true)][IO.DirectoryInfo]$path,
+    [Parameter(Position=2,Mandatory=$true)][string]$modulesPath
+  )
+  $targetDirectory = Join-Path $modulesDirectory $name
+  Create-DirectoryIfNeeded $modulesDirectory
+  Create-DirectoryIfNeeded $targetDirectory
+  Copy-Item $path\* $targetDirectory -Recurse -Force 
+}
+
+function Get-ModulesDirectoryForCurrentUser {
+  $modulesDirectory = Join-Path ([Environment]::GetFolderPath("MyDocuments")) WindowsPowerShell\Modules
+  $modulesDirectory
+}
+
+function Create-DirectoryIfNeeded {
+  param($path)
+  if(!(Test-Path $path)) {
+    New-Item -path $path -ItemType Directory -Force
+  }
+}
+
+function Assert-PathExists {
+  param($path)
+  if(!(Test-Path $path)) {
+    throw ("The path does not exist: $path.")
+  }
+}
+
+function Remove-ItemRecurseForce {
+  param($path)
+  Remove-Item -Recurse -Force "$path"
+}
+
+Install-Module "Chewie" (Join-Path $toolsPath src)
 
 # remove everything as we have copied it over.
-Uninstall-Package Chewie
+$inVs = @(get-command Install-Package -ErrorAction SilentlyContinue).Length -eq 1
+if($inVs) {
+  Uninstall-Package Chewie
+} else {
+  Remove-ItemRecurseForce $toolsPath\..
+}
--- readme.md
+++ readme.md
@@ -5,3 +5,13 @@ To work on Chewie:
 
 ## To package Chewie
 .\build -T Package
+
+## Trying out chewie during 2.0.0 prerelease
+
+Add your directory with the nupkg to a custom feed.
+
+### To install Chewie inside of VS
+install-package chewie -version 2.0.0
+
+### To install Chewie outside of VS
+nuget install chewie -x -v 2.0.0;.\Chewie\tools\init.ps1 "" $pwd\Chewie\tools
