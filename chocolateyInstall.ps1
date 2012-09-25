3f1a49164e48fe3ac522ad03eae3d89b339019ea
Ian Davis <ian.f.davis@gmail.com>
2012-09-25 16:12:32 -0700

    Adding notes to chocolatey install script.

--- chocolateyInstall.ps1
+++ chocolateyInstall.ps1
@@ -1,3 +1,10 @@
 ﻿
+# From inside of VS we would run:
+#install-package chewie -version 2.0.0
+
+# From outside of VS, we would run:
+#nuget install chewie -x -v 2.0.0;.\Chewie\tools\init.ps1 "" $pwd\Chewie\tools
+
+# Todo: how to do this in chocolatey
 
 Write-ChocolateySuccess 'chewie'
