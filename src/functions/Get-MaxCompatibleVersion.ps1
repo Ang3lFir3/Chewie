648e1e76b9e3829f6994046e03ce376f45d10efb
Ian Davis <ian.f.davis@gmail.com>
2012-09-25 13:21:24 -0700

    Refactoring out ability to find the maximum compatible version based on a list of versions and a version spec.

---
+++ src/functions/Get-MaxCompatibleVersion.ps1
@@ -0,0 +1,13 @@
+﻿
+function Get-MaxCompatibleVersion {
+  [CmdletBinding()]
+  param(
+    [Parameter(Position=0,Mandatory=$true)][string]$packageName,
+    [Parameter(Position=1,Mandatory=$true)][AllowEmptyString()][string]$versionSpec
+  )
+  [xml]$targets = Get-PackageList $packageName
+  $versions = $targets.feed.entry | %{ (Get-VersionFromString $_.properties.version) }
+  $matchingVersions = $versions | ? { Test-VersionCompatibility $versionSpec $_.Version.ToString() }
+  $maxCompatibleVersion = Find-MaxVersion $matchingVersions
+  return $maxCompatibleVersion
+}
