e25f969e7d7b832adf88be30b1099246354dece5
Ian Davis <ian.f.davis@gmail.com>
2012-12-01 14:09:26 -0800

    Updating command line to remove ambiguity

--- src/chewie.ps1
+++ src/chewie.ps1
@@ -1,54 +1,85 @@
 function chewie {
-[CmdletBinding(DefaultParameterSetName='default')]
-param(
+  [CmdletBinding()]
+  param(
+  [Parameter(Position=0,Mandatory=$true,ParameterSetName='install')]
+  [switch] $install = $false,
+  [Parameter(Position=0,Mandatory=$true,ParameterSetName='update')]
+  [switch] $update = $false,
+  [Parameter(Position=0,Mandatory=$true,ParameterSetName='uninstall')]
+  [switch] $uninstall = $false,
+  [Parameter(Position=0,Mandatory=$true,ParameterSetName='outdated')]
+  [switch] $outdated = $false,
+  [Parameter(Position=0,Mandatory=$true,ParameterSetName='init')]
+  [switch] $init = $false,
+  [Parameter(Position=0,Mandatory=$true,ParameterSetName='convert')]
+  [switch] $convert = $false,
+  [Parameter(Position=0,Mandatory=$true,ParameterSetName='downloadNuGet')]
+  [switch] $downloadNuGet = $false,
   [Parameter(ParameterSetName='install')]
   [Parameter(ParameterSetName='update')]
   [Parameter(ParameterSetName='uninstall')]
   [Parameter(ParameterSetName='outdated')]
-  [Parameter(ParameterSetName='convert')]
-  [Parameter(ParameterSetName='help')]
-  [Parameter(ParameterSetName='default')]
-  [Parameter(Position=0,Mandatory=$false,HelpMessage="You must specify which task to execute.")]
-  [ValidateSet('install','update', 'uninstall', 'outdated', 'init', 'help', '?', 'convert')]
-  [string] $task = "install",
-  [Parameter(Position=1, Mandatory=$false)]
-  [Parameter(ParameterSetName='install')]
-  [Parameter(ParameterSetName='update')]
-  [Parameter(ParameterSetName='uninstall')]
-  [Parameter(ParameterSetName='outdated')]
-  [Parameter(ParameterSetName='default')]
-  [string[]] $packageList = @(),
-  [Parameter(Position=2,Mandatory=$false,ParameterSetName='default')]
+  [Parameter(ParameterSetName='init')]
+  [AllowEmptyString()]
+  [AllowNull()]
+  [Parameter(Position=1,Mandatory=$false)]
   [string] $path,
-  [Parameter(Position=3,Mandatory=$false,ParameterSetName='default')]
+  [Parameter(ParameterSetName='install')]
+  [Parameter(ParameterSetName='update')]
+  [Parameter(ParameterSetName='uninstall')]
+  [Parameter(ParameterSetName='outdated')]
+  [Parameter(ParameterSetName='init')]
+  [AllowEmptyString()]
+  [AllowNull()]
+  [Parameter(Position=2,Mandatory=$false)]
   [string] $source,
-  [Parameter(Position=4,Mandatory=$false,ParameterSetName='default')]
+  [Parameter(ParameterSetName='install')]
+  [Parameter(ParameterSetName='update')]
+  [Parameter(ParameterSetName='uninstall')]
+  [Parameter(ParameterSetName='outdated')]
+  [Parameter(ParameterSetName='init')]
+  [AllowEmptyString()]
+  [AllowNull()]
+  [Parameter(Position=3,Mandatory=$false)]
   [string] $nugetFile = $null,
-  [Parameter(Position=2,Mandatory=$false,ParameterSetName='update')]
+  [Parameter(ParameterSetName='install')]
+  [Parameter(ParameterSetName='update')]
+  [Parameter(ParameterSetName='uninstall')]
+  [Parameter(ParameterSetName='outdated')]
+  [Parameter(Position=4, Mandatory=$false)]
+  [AllowEmptyString()]
+  [AllowNull()]
+  [string]$package = "",
+  [Parameter(Position=1,Mandatory=$false,ParameterSetName='update')]
   [switch] $self,
-  [Parameter(Position=2,Mandatory=$false,ParameterSetName='outdated')]
+  [Parameter(Position=1,Mandatory=$false,ParameterSetName='outdated')]
   [switch] $pre,
-  [Parameter(Position=2,Mandatory=$false,ParameterSetName='convert')]
+  [Parameter(Position=1,Mandatory=$false,ParameterSetName='convert')]
   [switch] $applyChanges,
-  [Parameter(Position=0,Mandatory=$false,ParameterSetName='getnuget')]
-  [switch] $downloadNuGet,
-  [Parameter(Position=0,Mandatory=$false,ParameterSetName='help')]
-  [switch][alias("?")] $help = $false,
-  [Parameter(Position=5,Mandatory=$false)]
+  [Parameter(ParameterSetName='install')]
+  [Parameter(ParameterSetName='update')]
+  [Parameter(ParameterSetName='uninstall')]
+  [Parameter(ParameterSetName='outdated')]
+  [Parameter(ParameterSetName='init')]
+  [Parameter(ParameterSetName='convert')]
+  [Parameter(ParameterSetName='downloadNuGet')]
+  [Parameter(Mandatory=$false)]
   [switch] $nologo = $false
   )
+  $task = $PSCmdlet.ParameterSetName
   $here = (Split-Path -parent $script:MyInvocation.MyCommand.path)
-  
+
   if(!$skipFileLoading) {
     Resolve-Path $here\functions\*.ps1 | % { Write-Host "Loading $_"; . $_.ProviderPath }
   }
 
   Load-Configuration
-  Write-Debug "Parameter Set Chosen $($PSCmdlet.ParameterSetName)"
-
-  if ($debug) {
+  
+  if ($PSBoundParameters['Debug']) {
     $chewie.DebugPreference = "Continue"
     $chewie.verboseError = $true
+    $PSBoundParameters.Keys | % { Write-ColoredOutput "$_ $($PSBoundParameters[$_])" -foregroundcolor Yellow }
+    Write-ColoredOutput "Parameter Set Chosen $($PSCmdlet.ParameterSetName)" -foregroundcolor Yellow
   }
 
   if (-not $nologo) {
@@ -62,7 +93,7 @@ param(
 
   if($downloadNuGet) {
     $webClient = New-Object System.Net.WebClient
-    $webClient.DownloadFile("https://nuget.org/nuget.exe", "$here\NuGet.exe");
+    $webClient.DownloadFile("https://nuget.org/nuget.exe", (Join-Path $here "NuGet.exe"));
     return
   }
 
@@ -72,10 +103,18 @@ param(
     if(!(Test-Path $chewie.nugetFile)) {
       Write-Output "Creating $($chewie.nugetFile)"
       New-Item $chewie.nugetFile -ItemType File | Out-Null
-      Add-Content $chewie.nugetFile "install_to 'lib'"
+      if($source) {
+        Add-Content $chewie.nugetFile "source '$source'"
+      }
+      if($path) {
+        Add-Content $chewie.nugetFile "install_to '$path'"
+      } else {
+        Add-Content $chewie.nugetFile "install_to 'lib'"
+      }
+      if($package) {
+        Add-Content $chewie.nugetFile "chew '$package'"
+      }
     }
-
-    $packageList | % { Add-Content $chewie.nugetFile "chew '$_'" }
     return
   }
 
@@ -86,8 +125,8 @@ param(
     ConvertTo-Chewie $path $applyChanges
     return
   }
-  Write-Debug "Invoke-Chewie $task $packageList $without"
-  Invoke-Chewie $task $packageList $without
+  Write-Debug "Invoke-Chewie $task $package $without"
+  Invoke-Chewie $task $package $without
 }
 
 chewie @args
